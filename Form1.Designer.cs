aea10dc88ff519a8e7b5930a9fe50bd48422dde6
chucklay <chucklay@yahoo.com>
2016-05-19 12:39:44 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Generalized_Backup_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Game 1"}, -1, System.Drawing.Color.Empty, System.Drawing.Color.Empty, new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.GameList = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.GameList);
+            this.splitContainer1.Size = new System.Drawing.Size(684, 511);
+            this.splitContainer1.SplitterDistance = 228;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // GameList
+            // 
+            this.GameList.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.GameList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GameList.GridLines = true;
+            this.GameList.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.GameList.Location = new System.Drawing.Point(0, 0);
+            this.GameList.Name = "GameList";
+            this.GameList.Size = new System.Drawing.Size(228, 511);
+            this.GameList.TabIndex = 0;
+            this.GameList.UseCompatibleStateImageBehavior = false;
+            this.GameList.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(684, 511);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Generalized Backup Tool";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView GameList;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generalized_Backup_Tool
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Attempt to load configuration
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    class Defaults{
+        private string gamePath = "";
+        private string backupPath = "";
+        private int numSaves = 10;
+        private int minutesBetweenSaves = 10;
+
+        public Defaults()
+        {
+
+        }
+
+        public string getGamePath()
+        {
+            return gamePath;
+        }
+        public string getBackupPath()
+        {
+            return backupPath;
+        }
+        public int getNumSaves()
+        {
+            return numSaves;
+        }
+        public int getMinutesBetweenSaves()
+        {
+            return minutesBetweenSaves;
+        }
+
+        public void setGamePath(string newPath)
+        {
+
+        }
+
+    }
+}
