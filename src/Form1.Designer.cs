de38b8a282cf68e98f5b346a2e746099ccba2af7
chucklay <chucklay@yahoo.com>
2016-06-15 19:13:53 -0400

    Added game update form and organized file structure a bit. Need to update backup process for new file list structure.

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Generalized_Backup_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.addGameButton = new System.Windows.Forms.Button();
+            this.GameList = new System.Windows.Forms.ListView();
+            this.gameName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.gameImage = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.saveChangesButton = new System.Windows.Forms.Button();
+            this.processNameTextBox = new System.Windows.Forms.TextBox();
+            this.processNameLabel = new System.Windows.Forms.Label();
+            this.saveFilesTextBox = new System.Windows.Forms.TextBox();
+            this.saveFilesLabel = new System.Windows.Forms.Label();
+            this.savePathTextBox = new System.Windows.Forms.TextBox();
+            this.savePathLabel = new System.Windows.Forms.Label();
+            this.gameNameTextBox = new System.Windows.Forms.TextBox();
+            this.gameNameChangeLabel = new System.Windows.Forms.Label();
+            this.GameLabel = new System.Windows.Forms.Label();
+            this.pathBrowseButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.addGameButton);
+            this.splitContainer1.Panel1.Controls.Add(this.GameList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pathBrowseButton);
+            this.splitContainer1.Panel2.Controls.Add(this.saveChangesButton);
+            this.splitContainer1.Panel2.Controls.Add(this.processNameTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.processNameLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.saveFilesTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.saveFilesLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.savePathTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.savePathLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.gameNameTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.gameNameChangeLabel);
+            this.splitContainer1.Panel2.Controls.Add(this.GameLabel);
+            this.splitContainer1.Size = new System.Drawing.Size(948, 560);
+            this.splitContainer1.SplitterDistance = 205;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // addGameButton
+            // 
+            this.addGameButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addGameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addGameButton.Location = new System.Drawing.Point(0, 514);
+            this.addGameButton.Name = "addGameButton";
+            this.addGameButton.Size = new System.Drawing.Size(202, 46);
+            this.addGameButton.TabIndex = 1;
+            this.addGameButton.Text = "Add Game";
+            this.addGameButton.UseVisualStyleBackColor = true;
+            this.addGameButton.Click += new System.EventHandler(this.addGameButton_Click);
+            // 
+            // GameList
+            // 
+            this.GameList.Alignment = System.Windows.Forms.ListViewAlignment.Default;
+            this.GameList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GameList.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.GameList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.gameName,
+            this.gameImage});
+            this.GameList.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameList.GridLines = true;
+            this.GameList.Location = new System.Drawing.Point(0, 0);
+            this.GameList.MultiSelect = false;
+            this.GameList.Name = "GameList";
+            this.GameList.Size = new System.Drawing.Size(205, 508);
+            this.GameList.TabIndex = 0;
+            this.GameList.UseCompatibleStateImageBehavior = false;
+            this.GameList.View = System.Windows.Forms.View.List;
+            this.GameList.SelectedIndexChanged += new System.EventHandler(this.GameList_SelectedIndexChanged);
+            // 
+            // gameName
+            // 
+            this.gameName.DisplayIndex = 1;
+            this.gameName.Text = "Game Name";
+            this.gameName.Width = 332;
+            // 
+            // gameImage
+            // 
+            this.gameImage.DisplayIndex = 0;
+            this.gameImage.Text = "Game Image";
+            this.gameImage.Width = 88;
+            // 
+            // saveChangesButton
+            // 
+            this.saveChangesButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveChangesButton.Enabled = false;
+            this.saveChangesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveChangesButton.Location = new System.Drawing.Point(3, 514);
+            this.saveChangesButton.Name = "saveChangesButton";
+            this.saveChangesButton.Size = new System.Drawing.Size(733, 46);
+            this.saveChangesButton.TabIndex = 9;
+            this.saveChangesButton.Text = "Save Changes";
+            this.saveChangesButton.UseVisualStyleBackColor = true;
+            this.saveChangesButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // processNameTextBox
+            // 
+            this.processNameTextBox.Location = new System.Drawing.Point(14, 212);
+            this.processNameTextBox.Name = "processNameTextBox";
+            this.processNameTextBox.Size = new System.Drawing.Size(294, 20);
+            this.processNameTextBox.TabIndex = 8;
+            this.processNameTextBox.Visible = false;
+            // 
+            // processNameLabel
+            // 
+            this.processNameLabel.AutoSize = true;
+            this.processNameLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.processNameLabel.Location = new System.Drawing.Point(10, 190);
+            this.processNameLabel.Name = "processNameLabel";
+            this.processNameLabel.Size = new System.Drawing.Size(118, 19);
+            this.processNameLabel.TabIndex = 7;
+            this.processNameLabel.Text = "Process name";
+            this.processNameLabel.Visible = false;
+            // 
+            // saveFilesTextBox
+            // 
+            this.saveFilesTextBox.Location = new System.Drawing.Point(14, 167);
+            this.saveFilesTextBox.Name = "saveFilesTextBox";
+            this.saveFilesTextBox.Size = new System.Drawing.Size(294, 20);
+            this.saveFilesTextBox.TabIndex = 6;
+            this.saveFilesTextBox.Visible = false;
+            // 
+            // saveFilesLabel
+            // 
+            this.saveFilesLabel.AutoSize = true;
+            this.saveFilesLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveFilesLabel.Location = new System.Drawing.Point(10, 145);
+            this.saveFilesLabel.Name = "saveFilesLabel";
+            this.saveFilesLabel.Size = new System.Drawing.Size(435, 19);
+            this.saveFilesLabel.TabIndex = 5;
+            this.saveFilesLabel.Text = "Save file names (Blank to back up all, comma separated)";
+            this.saveFilesLabel.Visible = false;
+            // 
+            // savePathTextBox
+            // 
+            this.savePathTextBox.Enabled = false;
+            this.savePathTextBox.Location = new System.Drawing.Point(14, 122);
+            this.savePathTextBox.Name = "savePathTextBox";
+            this.savePathTextBox.Size = new System.Drawing.Size(294, 20);
+            this.savePathTextBox.TabIndex = 4;
+            this.savePathTextBox.Visible = false;
+            // 
+            // savePathLabel
+            // 
+            this.savePathLabel.AutoSize = true;
+            this.savePathLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savePathLabel.Location = new System.Drawing.Point(10, 100);
+            this.savePathLabel.Name = "savePathLabel";
+            this.savePathLabel.Size = new System.Drawing.Size(86, 19);
+            this.savePathLabel.TabIndex = 3;
+            this.savePathLabel.Text = "Save Path";
+            this.savePathLabel.Visible = false;
+            // 
+            // gameNameTextBox
+            // 
+            this.gameNameTextBox.Location = new System.Drawing.Point(14, 77);
+            this.gameNameTextBox.Name = "gameNameTextBox";
+            this.gameNameTextBox.Size = new System.Drawing.Size(294, 20);
+            this.gameNameTextBox.TabIndex = 2;
+            this.gameNameTextBox.Visible = false;
+            // 
+            // gameNameChangeLabel
+            // 
+            this.gameNameChangeLabel.AutoSize = true;
+            this.gameNameChangeLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameNameChangeLabel.Location = new System.Drawing.Point(10, 54);
+            this.gameNameChangeLabel.Name = "gameNameChangeLabel";
+            this.gameNameChangeLabel.Size = new System.Drawing.Size(101, 19);
+            this.gameNameChangeLabel.TabIndex = 1;
+            this.gameNameChangeLabel.Text = "Game Name";
+            this.gameNameChangeLabel.Visible = false;
+            // 
+            // GameLabel
+            // 
+            this.GameLabel.AutoSize = true;
+            this.GameLabel.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameLabel.Location = new System.Drawing.Point(3, 9);
+            this.GameLabel.Name = "GameLabel";
+            this.GameLabel.Size = new System.Drawing.Size(305, 41);
+            this.GameLabel.TabIndex = 0;
+            this.GameLabel.Text = "Game Name Here";
+            this.GameLabel.Visible = false;
+            // 
+            // pathBrowseButton
+            // 
+            this.pathBrowseButton.Location = new System.Drawing.Point(314, 122);
+            this.pathBrowseButton.Name = "pathBrowseButton";
+            this.pathBrowseButton.Size = new System.Drawing.Size(99, 20);
+            this.pathBrowseButton.TabIndex = 10;
+            this.pathBrowseButton.Text = "Browse";
+            this.pathBrowseButton.UseVisualStyleBackColor = true;
+            this.pathBrowseButton.Visible = false;
+            this.pathBrowseButton.Click += new System.EventHandler(this.pathBrowseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(948, 560);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Generalized Backup Tool";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView GameList;
+        private System.Windows.Forms.Button addGameButton;
+        private System.Windows.Forms.ColumnHeader gameName;
+        private System.Windows.Forms.ColumnHeader gameImage;
+        private System.Windows.Forms.TextBox gameNameTextBox;
+        private System.Windows.Forms.Label gameNameChangeLabel;
+        private System.Windows.Forms.Label GameLabel;
+        private System.Windows.Forms.Label saveFilesLabel;
+        private System.Windows.Forms.TextBox savePathTextBox;
+        private System.Windows.Forms.Label savePathLabel;
+        private System.Windows.Forms.TextBox saveFilesTextBox;
+        private System.Windows.Forms.Label processNameLabel;
+        private System.Windows.Forms.TextBox processNameTextBox;
+        private System.Windows.Forms.Button saveChangesButton;
+        private System.Windows.Forms.Button pathBrowseButton;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generalized_Backup_Tool
+{
+    public partial class Form1 : Form
+    {
+        ImageList imgList = new ImageList();
+        public Form1()
+        {
+            InitializeComponent();
+            refreshGamesList();
+        }
+
+        private void GameList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //TODO Change the game details here.
+            ListView.SelectedIndexCollection items = this.GameList.SelectedIndices;
+            foreach(int idex in items)
+            {
+                setMainPannel(Program.gameList.ElementAt(idex));
+            }
+        }
+
+        private void addGameButton_Click(object sender, EventArgs e)
+        {
+            AddGame add = new AddGame();
+            add.Show();
+        }
+
+        private void setMainPannel(Game game)
+        {
+            System.Console.WriteLine("Selected game: " + game.gameName);
+
+            string fileNames = "";
+            if (game.fileNames != null)
+            {
+                fileNames = game.fileNames.ElementAt(0);
+                for(int i = 1; i < game.fileNames.Count; i++)
+                {
+                    fileNames = fileNames + ", " + game.fileNames.ElementAt(i);
+                }
+            }
+
+            this.GameLabel.Text = game.gameName;
+            this.gameNameTextBox.Text = game.gameName;
+            this.savePathTextBox.Text = game.savePath;
+            this.saveFilesTextBox.Text = fileNames;
+            this.processNameTextBox.Text = game.processName;
+
+            this.GameLabel.Visible = true;
+            this.gameNameChangeLabel.Visible = true;
+            this.gameNameTextBox.Visible = true;
+            this.savePathLabel.Visible = true;
+            this.savePathTextBox.Visible = true;
+            this.pathBrowseButton.Visible = true;
+            this.saveFilesLabel.Visible = true;
+            this.saveFilesTextBox.Visible = true;
+            this.processNameLabel.Visible = true;
+            this.processNameTextBox.Visible = true;
+            this.saveChangesButton.Enabled = true;
+        }
+
+        private void refreshGamesList()
+        {
+            for (int i = 0; i < Program.gameList.Count; i++)
+            {
+                // TODO Allow the user to set an image for each game.
+                // Right now, this is just a placeholder image.
+                imgList.Images.Add(Image.FromStream(System.Reflection.Assembly.GetExecutingAssembly()
+                    .GetManifestResourceStream("Generalized_Backup_Tool.res.questionmark.png")));
+            }
+
+            imgList.ImageSize = new Size(50, 50);
+
+            this.GameList.LargeImageList = imgList;
+            this.GameList.SmallImageList = imgList;
+
+            GameList.Items.Clear();
+
+            for (int i = 0; i < Program.gameList.Count; i++)
+            {
+                this.GameList.Items.Add(new ListViewItem { ImageIndex = i, Text = 
+                    Program.gameList.ElementAt(i).gameName });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (validateInput())
+            {
+                saveChanges();
+                Program.writeGameList();
+                MessageBox.Show("Game updated!", "Success!", MessageBoxButtons.OK);
+            }
+        }
+
+        private void saveChanges()
+        {
+            ListView.SelectedIndexCollection items = this.GameList.SelectedIndices;
+            int gameIndex = 0;
+            foreach (int i in items)
+            {
+                gameIndex = i;
+            }
+            Program.gameList.ElementAt(gameIndex).gameName = this.gameNameTextBox.Text;
+            Program.gameList.ElementAt(gameIndex).savePath = this.savePathTextBox.Text;
+            if (this.saveFilesTextBox.Text.Equals(""))
+            {
+                Program.gameList.ElementAt(gameIndex).fileNames = null;
+            }
+            else
+            {
+                string fullList = this.saveFilesTextBox.Text;
+                string[] split = fullList.Split(',');
+                for(int i = 0; i < split.Length; i++)
+                {
+                    split[i].Trim();
+                }
+                Program.gameList.ElementAt(gameIndex).fileNames = new List<string>(split);
+            }
+            Program.gameList.ElementAt(gameIndex).processName = this.processNameTextBox.Text;
+        }
+
+        private bool validateInput()
+        {
+            // First, check game name for any invalid characters:
+            string name = this.gameNameTextBox.Text;
+            foreach(char invalChar in System.IO.Path.GetInvalidPathChars())
+            {
+                if (name.Contains(invalChar))
+                {
+                    // Name contains an invalid character!
+                    this.gameNameTextBox.Text = "Game name cannot be used as directory!";
+                    this.gameNameTextBox.BackColor = Color.Red;
+                    return false;
+                }
+            }
+            // Check Make sure all files are actually in the directory.
+            string fileString = this.saveFilesTextBox.Text;
+            // The user may want to back up all files in the directory.
+            System.Console.WriteLine("File text: \"" + this.saveFilesTextBox.Text + "\"");
+            if (!fileString.Equals(""))
+            {
+                string[] files = fileString.Split(',');
+                foreach(string fileName in files)
+                {
+                    //Make sure there are no invalid characters.
+                    foreach(char invalChar in System.IO.Path.GetInvalidFileNameChars())
+                    {
+                        if (fileName.Contains(invalChar))
+                        {
+                            this.saveFilesTextBox.Text = "Files contain an invalid character!";
+                            this.saveFilesTextBox.BackColor = Color.Red;
+                            return false;
+                        }
+                    }
+                    // Check to make sure each file exists.
+                    if(!System.IO.File.Exists(System.IO.Path.Combine(this.savePathTextBox.Text, fileName)))
+                    {
+                        this.saveFilesTextBox.Text = "One or more files do not exist!";
+                        this.saveFilesTextBox.BackColor = Color.Red;
+                        return false;
+                    }
+                }
+            }
+            // Delete the ".exe" from the process name, if it exists.
+            string pathName = this.processNameTextBox.Text;
+            if (pathName.EndsWith(".exe"))
+            {
+                string[] pathNameSplit = pathName.Split('.');
+                pathName = pathNameSplit[0];
+                for(int i = 1; i < pathNameSplit.Length-1; i++)
+                {
+                    pathName = pathName + "." + pathNameSplit[i];
+                }
+                this.processNameTextBox.Text = pathName;
+            }
+            return true;
+        }
+
+        private void pathBrowseButton_Click(object sender, EventArgs e)
+        {
+            // Open a FolderBrowserDialog to get to the game's save path.
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                this.savePathTextBox.Text = fbd.SelectedPath;
+            }
+        }
+    }
+}
